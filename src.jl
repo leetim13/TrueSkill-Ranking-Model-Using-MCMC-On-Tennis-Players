1d4210f6e885d7ac533b59c9bf62836f19d1e8b8
Timothy Lee <hwtimothylee@yahoo.com>
2021-01-12 20:08:59 -0500

    Add files via upload

---
+++ src.jl
@@ -0,0 +1,37 @@
+module A2funcs
+using Plots
+using StatsFuns: log1pexp
+
+
+function factorized_gaussian_log_density(mu,logsig,xs)
+  """
+  mu and logsig either same size as x in batch or same as whole batch
+  returns a 1 x batchsize array of likelihoods
+  """
+  σ = exp.(logsig)
+  return sum((-1/2)*log.(2π*σ.^2) .+ -1/2 * ((xs .- mu).^2)./(σ.^2),dims=1)
+end
+
+function skillcontour!(f; colour=nothing)
+  n = 100
+  x = range(-3,stop=3,length=n)
+  y = range(-3,stop=3,length=n)
+  z_grid = Iterators.product(x,y) # meshgrid for contour
+  z_grid = reshape.(collect.(z_grid),:,1) # add single batch dim
+  z = f.(z_grid)
+  z = getindex.(z,1)'
+  max_z = maximum(z)
+  levels = [.99, 0.9, 0.8, 0.7,0.6,0.5, 0.4, 0.3, 0.2] .* max_z
+  if colour==nothing
+  p1 = contour!(x, y, z, fill=false, levels=levels)
+  else
+  p1 = contour!(x, y, z, fill=false, c=colour,levels=levels,colorbar=false)
+  end
+  plot!(p1)
+end
+
+function plot_line_equal_skill!()
+  plot!(range(-3, 3, length=200), range(-3, 3, length=200), label="Equal Skill")
+end
+
+end
